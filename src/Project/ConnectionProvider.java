2e37c1592e68ae9824e4a0c6aa79c7a2026b6e2b
Nhantoetsocute <nguyenbanhan017@gmail.com>
2025-10-24 22:15:33 +0700

    nhanupfile

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+import java.sql.DriverManager;
+/**
+ *
+ * @author nguyenbanhan
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try 
+        {
+          
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hostel","root","12345678");
+            return con;
+        } catch (SQLException e) 
+        {
+            System.out.println("Err"+ e.getMessage());
+            return null;
+        }
+    }
+}
